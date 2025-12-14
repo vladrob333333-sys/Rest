ada85388631e675a6ff5fab81f5776505a452eda
vladrob333333-sys <vladrob333333@gmail.com>
2025-12-14 14:49:58 +0300

    wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 from app import app, init_db
 
-# Инициализация базы данных
-with app.app_context():
-    init_db()
-
-if __name__ == '__main__':
-    app.run()
+# Инициализация базы данных при запуске
+if __name__ == "__main__":
+    with app.app_context():
+        init_db()
+    
